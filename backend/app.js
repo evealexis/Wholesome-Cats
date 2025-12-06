3f64ec36dd98b5bfb57d233512973efcd1bae09c
evealexis <162702324+evealexis@users.noreply.github.com>
2025-12-06 18:30:09 +0000

    Update backend to serve cat API URL and modify frontend to fetch from backend

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json({message: 'Hello World'});
+app.get("/", async (req, res) => {
+
+    const num = Math.floor(Math.random() * 999);
+  const url = `http://cataas.com/cat?t=${num}`;
+  res.json({ catURL: url });
+
 });
 
 
