73237c09dae3bc37d5ca10c07aa7aa44f7cdcec4
evealexis <alicia.e.alexis@gmail.com>
2026-06-30 21:26:21 +0100

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,5 @@
-import express from 'express';
-
-const app = express();
-
-
-app.get("/", async (req, res) => {
-  const url = `http://cataas.com/cat?t=${Math.floor(Math.random() * 9999)}`;
+export default function handler (req, res) {
+  const url = `https://cataas.com/cat?t=${Math.floor(Math.random() * 9999)}`;
   res.json({ catURL: url });
 
-});
-
-export default app;
+};
